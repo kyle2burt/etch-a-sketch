27382c42aaecb500b19a6f107dff472641d37b3f
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-03 22:05:43 -0800

    Add 16x16 grid of divs and basic styles
    16x16 grid is created using javascript and basic style is in the css

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log('hello world');
+
+const gridContainer = document.querySelector('#grid-container');
+
+for (i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('item-container');
+    for (j = 0; j < 16; j++) {
+        const column = document.createElement('div');
+        column.classList.add('item');
+        row.appendChild(column);
+    }
+    gridContainer.appendChild(row);
+}
