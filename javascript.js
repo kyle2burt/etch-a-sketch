d4cf39c8e34816cb3a24fc8f42e9debb2c8bcc25
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-03 23:11:53 -0800

    Add button that lets you control grid size

--- javascript.js
+++ javascript.js
@@ -1,20 +1,40 @@
 console.log('hello world');
 
 const gridContainer = document.querySelector('#grid-container');
+const button = document.querySelector('#resize');
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('item-container');
-    for (j = 0; j < 16; j++) {
-        const column = document.createElement('div');
-        column.classList.add('item');
-        row.appendChild(column);
-    }
-    gridContainer.appendChild(row);
-}
+createGrid(16);
 
 gridContainer.addEventListener('mouseover', (event) => {
     if (event.target != gridContainer) {
         event.target.style.backgroundColor = "red";
     }
 });
+
+button.addEventListener('click', (event) => {
+    let size = prompt("Enter grid size (16-100):");
+    if (size >= 100) size = 100;
+    else if (size <= 16) size = 16;
+    removeGrid();
+    createGrid(size);
+});
+
+function createGrid(size) {
+    for (i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('item-container');
+        for (j = 0; j < size; j++) {
+            const column = document.createElement('div');
+            column.classList.add('item');
+            row.appendChild(column);
+        }
+        gridContainer.appendChild(row);
+    }
+}
+
+function removeGrid() {
+    console.log(gridContainer.childNodes.length)
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
